ab915b13317e9424f90218aa94a1d29dfaad35d9
Ophélien Fouques <ophelien.fouques@viacesi.fr>
2022-05-03 21:07:29 +0200

    first commit
    Signed-off-by: Ophélien Fouques <ophelien.fouques@viacesi.fr>

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <b-form-input class="mr-sm-2" placeholder="Search" size="sm"></b-form-input>
+              <b-button class="my-2 my-sm-0" size="sm" type="submit">Search</b-button>
+            </b-nav-form>
+
+            <b-navbar-nav>
+              <b-nav-item href="#">Link</b-nav-item>
+            </b-navbar-nav>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style>
+  body {
+    background-color: #F2F2F2;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+  }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello container">
+    <h1>{{ msg }}</h1>
+    <div class="row tiles">
+      <div class="col tile">
+        <img alt="" src="">
+        <h3>Product name</h3>
+      </div>
+      <div class="col tile">
+        2 of 2
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
